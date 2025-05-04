7d226883be660db6819557df4153a80ff11ec098
Amir Azadeh Ranjbar <amirazadehranjbar@gmail.com>
2025-05-04 10:01:05 -0800

    commit 2

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const errorHandler = require("./handlers/errorHandler");
 
 const authRoutes = require("./routes/authRoutes");
 
 
+app.use(cors(
+
+    {
+        origin: process.env.CORS_ORIGIN,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    }
+
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
