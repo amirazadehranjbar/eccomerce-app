cb6eaa28c34967167b0e941498e7870760d77cf6
Amir Azadeh Ranjbar <amirazadehranjbar@gmail.com>
2025-05-04 10:28:46 -0800

    make change for test vertest deploy after switching branch to master

--- backend/app.js
+++ backend/app.js
@@ -26,4 +26,7 @@ app.use("/",authRoutes);
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)});
+    console.log(`Server is running on port ${process.env.PORT}`)
+    console.log("Test deploy after changing branch to master");
+
+});
